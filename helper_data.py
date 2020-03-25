89eda7ab28e36621c156ea1a210c48c4cef19ef7
Devin Datt <42626142+devindatt@users.noreply.github.com>
2020-03-25 17:29:21 -0400

    Upload initial code and help file

---
+++ helper_data.py
@@ -0,0 +1,16 @@
+tract_dict = {
+        'cycle': [(477, 254, 55, 152), 'videos/cycle.mp4'] ,
+        'ship': [(751, 146, 51, 78), 'videos/drone-ship.mp4'],
+        'hockey': [(129, 47, 74, 85), 'videos/hockey.mp4'],
+        'face2' : [(237, 145, 74, 88), 'videos/face2.mp4'],
+        'meeting_CSRT': [(627, 183, 208, 190), 'videos/meeting.mp4'],    #CSRT
+        'meeting_KCF': [(652, 187, 118, 123), 'videos/meeting.mp4'],      #KCF
+        'surfing': [(97, 329, 118, 293), 'videos/surfing.mp4'],
+        'surf': [(548, 587, 52, 87), 'videos/surf.mp4'],
+        'spinning_red': [(232, 218, 377, 377), 'videos/spinning.mp4'],       #RED
+        'spinning_blue': [(699, 208, 383, 391), 'videos/spinning.mp4'],        #BLUE
+        'car': [(71, 457, 254, 188), 'videos/car.mp4']
+    } 
+
+
+tracker_types = ['BOOSTING', 'MIL','KCF', 'MEDIANFLOW', 'CSRT', 'MOSSE']
